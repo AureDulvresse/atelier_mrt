174cff82ffce0637aa26ca84c0bb4e33484f0b14
TheHIGHESTBoy <157598568+TheHIGHESTBoy@users.noreply.github.com>
2024-07-31 13:47:01 +0200

    d

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@
         define('DB_USER', 'root');
         define('DB_PASS', '');
 
-<<<<<<< HEAD
+
 // Créer une instance de PDO pour la connexion à la base de données
 try {
     $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
@@ -16,19 +16,3 @@ try {
 }
 
 ?> 
-=======
-        // Créer une instance de PDO pour la connexion à la base de données
-        try {
-            $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            die('Erreur de connexion à la base de données : ' . $e->getMessage());
-        }
-
-// Vérifiez que l'initialisation s'est bien déroulée
-// if ($auth) {
-//     echo 'PHPAuth a été initialisé avec succès.';
-// } else {
-//     echo 'Erreur lors de l\'initialisation de PHPAuth.';
-// }
->>>>>>> 6e20f46b24cdd6fc27557e66104a2c78662aef5a
