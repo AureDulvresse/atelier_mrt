79dd187b0f7d9494802364a9d85c03cc6debc94c
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-07-31 14:05:15 +0100

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,12 +1,14 @@
-<!-- <?php
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
 
-        // Paramètres de la base de données
-        define('DB_HOST', 'localhost');
-        define('DB_NAME', 'ateliermrtdb');
-        define('DB_USER', 'root');
-        define('DB_PASS', '');
+use PHPAuth\Config as PHPAuthConfig;
+use PHPAuth\Auth as PHPAuth;
 
-
+// Paramètres de la base de données
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'ateliermrtdb');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 // Créer une instance de PDO pour la connexion à la base de données
 try {
     $pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
@@ -14,5 +16,12 @@ try {
 } catch (PDOException $e) {
     die('Erreur de connexion à la base de données : ' . $e->getMessage());
 }
+// Configuration de PHPAuth
+$config = new PHPAuthConfig($pdo);
+$auth = new PHPAuth($pdo, $config);
 
-?> 
+// if ($auth) {
+//     echo 'PHPAuth a été initialisé avec succès.';
+// } else {
+//     echo 'Erreur lors de l\'initialisation de PHPAuth.';
+// }
