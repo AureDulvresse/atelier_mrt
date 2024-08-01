165e93b6425b54dbceb22a1b1bdeee263ab71f6d
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-08-01 16:53:14 +0100

    Update db.sqlite3.sql

--- db.sqlite3.sql
+++ db.sqlite3.sql
@@ -35,10 +35,11 @@ CREATE TABLE customers (
     is_superuser BOOLEAN NOT NULL,
     first_name VARCHAR(150) NOT NULL,
     last_name VARCHAR(150) NOT NULL,
-    email VARCHAR(254) NOT NULL UNIQUE,
+    email VARCHAR(191) NOT NULL,
     is_staff BOOLEAN NOT NULL,
     is_active BOOLEAN NOT NULL,
-    date_joined DATETIME NOT NULL
+    date_joined DATETIME NOT NULL,
+    UNIQUE KEY unique_email (email(191))
 );
 
 
