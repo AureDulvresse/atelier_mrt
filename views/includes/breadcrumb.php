99e8fa8050746d9958721c204a58829efe6ed3af
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-08-03 06:55:45 +0100

    Update breadcrumb.php

--- views/includes/breadcrumb.php
+++ views/includes/breadcrumb.php
@@ -62,11 +62,11 @@
                             </li>
                             <?php if ($isLoggedIn) : ?>
                                 <!-- Utilisateur connecté -->
-                                <li><a href="cart" class="cart-icon"><i class='bx bx-cart'></i></a></li>
-                                <li><a href="profile" class="user-icon"><i class='bx bx-user'></i></a></li>
+                                <li><a href="/atelier_mrt/cart" class="cart-icon"><i class='bx bx-cart'></i></a></li>
+                                <li><a href="/atelier_mrt/profile" class="user-icon"><i class='bx bx-user'></i></a></li>
                             <?php else : ?>
                                 <!-- Utilisateur non connecté -->
-                                <li><a href="login" class="active">Connexion</a></li>
+                                <li><a href="/atelier_mrt/login" class="active">Connexion</a></li>
                             <?php endif; ?>
                         </ul>
                     </div>
