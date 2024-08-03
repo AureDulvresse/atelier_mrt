667c44213a2055ea55455f10ebd08d029e7e001f
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-08-03 07:46:14 +0100

    Update Cart.php

--- models/Cart.php
+++ models/Cart.php
@@ -19,7 +19,7 @@ class Cart
 
     public static function create($pdo, $customer_id)
     {
-        $query = "INSERT INTO cart SET customer_id=:customer_id";
+        $query = "INSERT INTO carts SET customer_id=:customer_id";
         // $stmt = $this->conn->prepare($query);
         $stmt = $pdo->prepare($query);
         $stmt->bindParam(':customer_id', $customer_id);
@@ -32,7 +32,7 @@ class Cart
 
     public static function get($pdo, $customer_id)
     {
-        $query = "SELECT id FROM cart WHERE customer_id = ?";
+        $query = "SELECT id FROM carts WHERE customer_id = ?";
         // $stmt = $this->conn->prepare($query);
         $stmt = $pdo->prepare($query);
         $stmt->bindParam(1, $customer_id);
