8e26e732dca50f820bbd741cf7f227ae9a06786f
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-08-05 16:35:04 +0100

    Update services.php

--- config/services.php
+++ config/services.php
@@ -6,14 +6,26 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
+$stripePublishableKey = $_ENV['STRIPE_PUBLISHABLE_KEY'];
+$stripeSecretKey = $_ENV['STRIPE_SECRET_KEY'];
+
+$paypalClientId = $_ENV['PAYPAL_CLIENT_ID'];
+$paypalSecret = $_ENV['PAYPAL_SECRET'];
+$paypalSandbox = $_ENV['PAYPAL_SANDBOX'];
+
+if (!$stripePublishableKey || !$stripeSecretKey || !$paypalClientId || !$paypalSecret || !$paypalSandbox === false) {
+    die('Erreur: Une ou plusieurs variables d\'environnement de la base de données ne sont pas définies.');
+}
+
+
 return [
     'stripe' => [
-        'secret_key' => $_ENV['STRIPE_SECRET_KEY'],
-        'publishable_key' => $_ENV['STRIPE_PUBLISHABLE_KEY'],
+        'secret_key' => $stripeSecretKey,
+        'publishable_key' => $stripePublishableKey,
     ],
     'paypal' => [
-        'client_id' => $_ENV['PAYPAL_CLIENT_ID'],
-        'secret' => $_env['PAYPAL_SECRET'],
-        'sandbox' => $_env['PAYPAL_SANDBOX'] === 'true',
+        'client_id' => $paypalClientId,
+        'secret' => $paypalSecret,
+        'sandbox' => $paypalSandbox === 'true',
     ],
 ];
