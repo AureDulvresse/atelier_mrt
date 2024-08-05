e769a93397107264053ab6e1f6c875e817966cf3
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-08-05 12:15:11 +0100

    Update cart.php

--- views/cart.php
+++ views/cart.php
@@ -54,14 +54,14 @@ include './views/includes/breadcrumb.php';
                     var_dump($user_cart);
                         foreach ($user_cart as $item) {
                             if (isset($item->artwork->id)) {
-                            $subtotal = $item->artwork->price * $quantity;
+                            $subtotal = $item->artwork->price * $item->quantity;
                             $totalPrice += $subtotal;
                     ?>
                             <tr>
                                 <td><img src="/assets/images/sample.jpg" alt="Artwork Image" class="cart-image" /></td>
                                 <td><?php echo $item->artwork->title; ?></td>
                                 <td><?php echo number_format($item->artwork->price, 2, ',', ' '); ?> €</td>
-                                <td><?php echo $quantity; ?></td>
+                                <td><?php echo $item->quantity; ?></td>
                                 <td><?php echo number_format($subtotal, 2, ',', ' '); ?> €</td>
                                 <td><a href="remove_from_cart.php?id=<?php echo $item->artwork_id; ?>" class="btn remove-from-cart">Retirer</a></td>
                             </tr>
