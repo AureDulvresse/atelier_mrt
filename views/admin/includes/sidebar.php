7b1e3c33c435bfd316b04c452a54c5fc15cc7c10
Aure <147180124+AureDulvresse@users.noreply.github.com>
2024-08-07 08:46:02 +0100

    Update sidebar.php

--- views/admin/includes/sidebar.php
+++ views/admin/includes/sidebar.php
@@ -31,13 +31,13 @@
                     <a class="nav-link active" href="/admin"><i class='bx bx-home'></i>Tableau de Bord</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/admin/artworks"><i class='bx bx-category'></i>Categorie</a>
+                    <a class="nav-link" href=""><i class='bx bx-category'></i>Categorie</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="../admin/artwork_page.php"><i class='bx bx-brush'></i>Techniques</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="../admin/artwork_page.php"><i class='bx bx-paint'></i>Mes œuvres</a>
+                    <a class="nav-link" href="/admin/artworks"><i class='bx bx-paint'></i>Mes œuvres</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="../admin/event_page.php"><i class='bx bx-calendar-event'></i>Événements</a>
